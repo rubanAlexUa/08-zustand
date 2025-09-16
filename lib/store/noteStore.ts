0397d6642c263b9551c08489832bb4d73b52c26f
Alex <rubashka2021@gmail.com>
2025-09-16 23:08:05 +0300

    add .

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { NoteFormValues } from "@/types/note";
 
 type NoteDraftStore = {
@@ -13,8 +14,16 @@ const initialDraft = {
   tag: "Todo" as const,
 };
 
-export const useNoteDraftStore = create<NoteDraftStore>()((set) => ({
-  draft: initialDraft,
-  setDraft: (note) => set(() => ({ draft: note })),
-  clearDraft: () => set(() => ({ draft: initialDraft })),
-}));
+export const useNoteDraftStore = create<NoteDraftStore>()(
+  persist(
+    (set) => ({
+      draft: initialDraft,
+      setDraft: (note) => set(() => ({ draft: note })),
+      clearDraft: () => set(() => ({ draft: initialDraft })),
+    }),
+    {
+      name: "note-draft",
+      partialize: (state) => ({ draft: state.draft }),
+    }
+  )
+);
